f8a6a69a1551e8d16fcd8efa8c713afa66861ae6
Christopher Roach <vthakr@gmail.com>
2014-03-16 17:05:13 -0700

    Added an install script

---
+++ install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+NENV_DIR="$HOME/.nenv"
+GITHUB_REPO_URL="https://github.com/croach/nenv.git"
+NENV_RAW_FILE_URL="https://raw.github.com/croach/nenv/master/nenv.sh"
+
+if ! hash git 2>/dev/null; then
+  if [ -d "$NENV_DIR" ]; then
+    echo "=> nenv is already installed in $NENV_DIR, trying to update"
+    echo -ne "\r=> "
+    cd $NENV_DIR && rm nenv.sh && curl -O $NENV_RAW_FILE_URL
+  else
+    # Cloning to $NENV_DIR
+    mkdir "$NENV_DIR" && cd "$NENV_DIR" && curl -O $NENV_RAW_FILE_URL
+  fi
+else
+  if [ -d "$NENV_DIR" ]; then
+    echo "=> nenv is already installed in $NENV_DIR, trying to update"
+    echo -ne "\r=> "
+    cd $NENV_DIR && git pull
+  else
+    # Cloning to $NENV_DIR
+    git clone $GITHUB_REPO_URL $NENV_DIR
+  fi
+fi
+
+# Detect profile file
+if [ ! -z "$1" ]; then
+  PROFILE="$1"
+else
+  if [ -f "$HOME/.bashrc" ]; then
+    PROFILE="$HOME/.bashrc"
+  elif [ -f "$HOME/.zshrc" ]; then
+    PROFILE="$HOME/.zshrc"
+  elif [ -f "$HOME/.profile" ]; then
+    PROFILE="$HOME/.profile"
+  fi
+fi
+
+SOURCE_STR=$(cat <<EOF
+if [[ -f "$HOME/.nenv/nenv.sh" ]]; then
+  source "$HOME/.nenv/nenv.sh"
+
+  # Uncomment the following line if you want virtual environments
+  # activated/deactivted as you cd into/out of them.
+  # alias cd="nenv_cd"
+
+  # Uncomment the following line if you want to try to check for a
+  # virtual environment in the current directory (and activate it)
+  # whenever a new shell session is created.
+  # nenv activate
+fi
+EOF
+)
+
+if [ -z "$PROFILE" ] || [ ! -f "$PROFILE" ] ; then
+  if [ -z $PROFILE ]; then
+    echo "=> Profile not found. Tried $HOME/.bash_profile and $HOME/.profile"
+  else
+    echo "=> Profile $PROFILE not found"
+  fi
+  echo "=> Run this script again after running the following:"
+  echo
+  echo "\ttouch $HOME/.profile"
+  echo
+  echo "-- OR --"
+  echo
+  echo "=> Append the following line to the correct file yourself"
+  echo
+  echo "$SOURCE_STR"
+  echo
+  echo "=> Close and reopen your terminal afterwards to start using nenv"
+  exit
+fi
+
+if ! grep -qc 'nenv.sh' $PROFILE; then
+  echo "=> Appending source string to $PROFILE"
+  echo "" >> "$PROFILE"
+  echo "$SOURCE_STR" >> "$PROFILE"
+else
+  echo "=> Source string already in $PROFILE"
+fi
+
+echo "=> Close and reopen your terminal to start using nenv"
+echo
+
